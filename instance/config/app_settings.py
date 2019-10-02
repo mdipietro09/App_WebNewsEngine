ac27cbd032e348c3ce5aa10f08f4d96436c8a71f
DI PIETRO MAURO <U382270@sede.corp.sanpaoloimi.com>
2019-10-02 08:43:54 +0200

    .

--- instance/config/app_settings.py
+++ instance/config/app_settings.py
@@ -10,8 +10,17 @@ debug = True
 ## dev
 #port = 5000
 
+#from instance.config.api_keys import *
+
 ## prod
 import os
 port = int(os.environ.get("PORT", 5000))
+
+import ast
+twitter_keys = ast.literal_eval(os.environ["twitter_keys"])
+newsapi_keys = ast.literal_eval(os.environ["newsapi_keys"])
+eventregistry_keys = ast.literal_eval(os.environ["eventregistry_keys"])
+
+
 
 
--- run_app.py
+++ run_app.py
@@ -4,17 +4,12 @@
 
 from instance.config.app_settings import *
 from instance.config.file_system import *
-#from instance.config.api_keys import *
 from app.server import server
 
 
-import os
-twitter_keys = os.environ["twitter_keys"]
-newsapi_keys = os.environ["newsapi_keys"]
-eventregistry_keys = os.environ["eventregistry_keys"]
-
 
 app = server.create_app(name=name,
-                        twitter_keys=twitter_keys, newsapi_keys=newsapi_keys, eventregistry_keys=eventregistry_keys)
+                        twitter_keys=twitter_keys, newsapi_keys=newsapi_keys, 
+                        eventregistry_keys=eventregistry_keys)
 
 app.run(host=host, port=port, threaded=threaded, debug=debug)
