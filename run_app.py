43b3bb4c534df7a6aa1423e93db50a4a2766222f
DI PIETRO MAURO <U382270@sede.corp.sanpaoloimi.com>
2019-10-02 08:24:03 +0200

    .

--- run_app.py
+++ run_app.py
@@ -4,9 +4,14 @@
 
 from instance.config.app_settings import *
 from instance.config.file_system import *
-from instance.config.api_keys import *
+#from instance.config.api_keys import *
 from app.server import server
 
+
+import os
+twitter_keys = os.environ["twitter_keys"]
+newsapi_keys = os.environ["newsapi_keys"]
+eventregistry_keys = os.environ["eventregistry_keys"]
 
 
 app = server.create_app(name=name,
