549c7134d3aebdfe9d91893b391ffabeb6e7731e
DI PIETRO MAURO <U382270@sede.corp.sanpaoloimi.com>
2019-10-04 12:18:09 +0200

    .

--- instance/config/app_settings.py
+++ instance/config/app_settings.py
@@ -5,21 +5,21 @@ host = "0.0.0.0"
 
 threaded = False
 
-debug = False
+debug = True
 
 ## dev
-#port = 5000
+port = 5000
 
-#from instance.config.api_keys import *
+from instance.config.api_keys import *
 
 ## prod
 import os
-port = int(os.environ.get("PORT", 5000))
+#port = int(os.environ.get("PORT", 5000))
 
 import ast
-twitter_keys = ast.literal_eval(os.environ["twitter_keys"])
-newsapi_keys = ast.literal_eval(os.environ["newsapi_keys"])
-eventregistry_keys = ast.literal_eval(os.environ["eventregistry_keys"])
+#twitter_keys = ast.literal_eval(os.environ["twitter_keys"])
+#newsapi_keys = ast.literal_eval(os.environ["newsapi_keys"])
+#eventregistry_keys = ast.literal_eval(os.environ["eventregistry_keys"])
 
 
 
